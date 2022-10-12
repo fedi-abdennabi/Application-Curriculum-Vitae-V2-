7acea7d0ef5aabdfeca5354df5178b9832a50bc6
Fedi_ <fedi.abdennabi@esprit.tn>
2022-10-12 22:15:23 +0100

    commit2

--- app/src/main/java/com/example/curriculumvitaekotlin/MainActivity.kt
+++ app/src/main/java/com/example/curriculumvitaekotlin/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.example.curriculumvitaekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.newSingleThreadContext
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var btnNext : Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editName = findViewById(R.id.etName)
+        editEmail = findViewById(R.id.email)
+        btnNext = findViewById(R.id.nextBTN)
+
+        btnNext.setOnClickListener{
+            startActivity(Intent(this,ThirdActivity::class.java)
+                .putExtra("name",editName.text.toString())
+                .putExtra("email",editEmail.text.toString()))
+        }
+
 
     }
 }
--- app/src/main/java/com/example/curriculumvitaekotlin/ThirdActivity.kt
+++ app/src/main/java/com/example/curriculumvitaekotlin/ThirdActivity.kt
@@ -2,10 +2,57 @@ package com.example.curriculumvitaekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.curriculumvitaekotlin.databinding.ActivityThirdBinding
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityThirdBinding
+    private lateinit var textName : TextView
+    private lateinit var textEmail : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        textName = findViewById(R.id.tv_name)
+        textEmail = findViewById(R.id.tv_email)
+
+        val Name = intent.getStringExtra("name")
+        val Email = intent.getStringExtra("email")
+
+
+        textName.text=Name
+        textEmail.text=Email
+
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnskills.setOnClickListener {
+            replaceFragment(Fragskills())
+        }
+        binding.btnhobbies.setOnClickListener {
+            replaceFragment(fraghobbies())
+        }
+        binding.btnlanguage.setOnClickListener {
+            replaceFragment(fraglanguage())
+        }
+
+        }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.framelayout,fragment)
+        fragmentTransaction.commit()
+
+
     }
-}
+
+
+    }
